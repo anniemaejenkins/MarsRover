414ba81a18b59c47dd20c48555eab6dd5c1da74e
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-07-26 14:02:40 -0400

    trying to figure out api key stuff

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,43 @@ import React, { Component } from 'react';
 import logo from './../logo.svg';
 import './../styles/App.css';
 
+const API_KEY = 'I55DSyUefgZ0HCHNpqhBWwipw1fitpPxdq65jJOE';
+const BASE_URL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/'
+//const imageUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rove}/photos?sol=${num}&camera=${cam}&api_key=${API_KEY}`;
+
+
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      camera: "",
+      rover: "",
+      number: "",
+      images: []
+    };
+  }
+
+  // componentDidMount() {
+  //   //let imageUrl = `${BASE_URL}${this.state.rover}/photos?sol=${this.state.number}&camera=${this.state.camera}&api_key=${API_KEY}`
+  // }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+
       </div>
     );
   }
 }
+
+// componentDidMount(){
+//   // fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/')
+//   // .then((result) => {
+//   //   result.json().then((response) => {
+//   //
+//   //   });
+//   // });
+// }
+
 
 export default App;
